50636a0d01253b80b3b04745f08096652315542b
juraantoniv <juraantoniv@gmail.com>
2023-02-08 15:15:58 +0000

    ok

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Car = ({car}) => {
-     const
+
+    const {brand,year,price}=car
 
     return (
         <div>
-            
+            <div>brand :{brand}</div>
+            <div>price: {price}</div>
+            <div>year: {year}</div>
+            <button>Update</button>
+            <button>Delete</button>
         </div>
     );
 };
