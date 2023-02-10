50149383ca3fb4225b11d59dd1b34e8fa1d5a49e
juraantoniv <juraantoniv@gmail.com>
2023-02-10 23:53:15 +0200

    ok

--- src/services/main.services/car.service.js
+++ src/services/main.services/car.service.js
@@ -2,7 +2,7 @@ import {axiosService} from "../api.Service";
 import {urls} from "../urls";
 
 export const carService ={
-    getAll:()=>axiosService.get(urls.cars.cars),
+    getAll:(page = 1)=>axiosService.get(urls.cars.cars, {params:{page}}),
     create:(data)=>axiosService.post(urls.cars.cars,data),
     updateById:(id,data)=>axiosService.put(urls.cars.byId(id),data),
     deleteById:(id)=>axiosService.delete(urls.cars.byId(id))
