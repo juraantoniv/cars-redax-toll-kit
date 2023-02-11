41488a7d2199e574b48bcd5751847f116e67eb05
juraantoniv <juraantoniv@gmail.com>
2023-02-11 13:55:35 +0200

    ok

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
+import {carService} from "../../services";
+import {useNavigate} from "react-router-dom";
 
 const RegisterPage = () => {
+
+    const {register, handleSubmit} = useForm();
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+
+    const registerUser = async (user) => {
+        try {
+            await carService.create(user)
+            navigate('/login')
+        } catch (e) {
+            setError(e.response.data)
+        }
+    };
+
     return (
         <div>
-            Register
+            <form onSubmit={handleSubmit(registerUser)}>
+                <input type="text" placeholder={'username'} {...register('username')}/>
+                <input type="text" placeholder={'password'} {...register('password')}/>
+                <button>Register</button>
+            </form>
+
+            {error&&<div>{JSON.stringify(error)}</div>}
         </div>
     );
 };
